f88c36f7f71437cd636150ff5bb3883de8f1c9a8
Ralica1 <rasim.korajlic@gmail.com>
2022-12-15 18:43:23 +0100

    Improved readability using ES6

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # Calculator
+
+Project to create **Calculator** game using only HTML, CSS and Vanilla JS.
+
+From The Odin Project's curriculum.
+
+## Built with
+
+### Technologies
+
+* HTML
+* CSS
+* JS
+
+### Tools
+
+* Visual Studio Code
+* Linux terminal
+* Git and GitHub
+## Summary
+### What I learned
+
+* Better knowledge of **Javascript** basics.
+* Improved **code structure** and **code readability**.
+* Improved **clean code** writing.
+* A lot of minor things.
+
+### What could be improved
+
+* Could add more functionality.
+* Cleaner code.
+* My commit messages are still not perfect.
--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,33 @@
-function addToInput(value) {
+const addToInput = value => {
     document.getElementById('input').value += value;
-  }
-
-  function calculate() {
-    var input = document.getElementById('input').value;
-    var result = 0;
-    var numbers = input.split(/\+|\-|\*|\//g);
-    var operations = input.replace(/[0-9]|\./g, '').split('');
-    var decimalAdded = false;
-
+  };
+  
+  const calculate = () => {
+    let input = document.getElementById('input').value;
+    let result = 0;
+    let numbers = input.split(/\+|\-|\*|\//g);
+    let operations = input.replace(/[0-9]|\./g, '').split('');
+    let decimalAdded = false;
+  
     // Perform multiplication and division first
-    for (var i = 0; i < operations.length; i++) {
+    for (let i = 0; i < operations.length; i++) {
       if (operations[i] === '*' || operations[i] === '/') {
-        var first = parseFloat(numbers[i]);
-        var second = parseFloat(numbers[i + 1]);
-
+        let first = parseFloat(numbers[i]);
+        let second = parseFloat(numbers[i + 1]);
+  
         if (operations[i] === '*') {
           result = first * second;
         } else {
           result = first / second;
         }
-
+  
         numbers.splice(i, 2, result);
         operations.splice(i, 1);
       }
     }
-
+  
     // Perform addition and subtraction
-    for (i = 0; i < operations.length; i++) {
+    for (let i = 0; i < operations.length; i++) {
       if (operations[i] === '+') {
         result = parseFloat(numbers[i]) + parseFloat(numbers[i + 1]);
         numbers.splice(i, 2, result);
@@ -38,13 +38,13 @@ function addToInput(value) {
         operations.splice(i, 1);
       }
     }
-
+  
     // If there are no more operations, return the result
     if (operations.length === 0) {
       document.getElementById('input').value = result;
     }
-  }
-
-  function clearInput() {
+  };
+  
+  const clearInput = () => {
     document.getElementById('input').value = '';
-  }
+  };
